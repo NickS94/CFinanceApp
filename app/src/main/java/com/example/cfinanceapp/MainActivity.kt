84af73eb969e66fdb4ac806c0adf1db866155f66
Nick Stavropoulos <stauropoulosn@gmail.com>
2024-04-16 16:18:48 +0200

    Bottom Navigation View Nav Graph

--- app/src/main/java/com/example/cfinanceapp/MainActivity.kt
+++ app/src/main/java/com/example/cfinanceapp/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.cfinanceapp
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.cfinanceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        viewBinding.bottomNavigationView.setupWithNavController(navHost.navController)
+
+
+        navHost.navController.addOnDestinationChangedListener{_,destination,_->
+            when(destination.id) {
+                R.id.registerFragment -> viewBinding.bottomNavigationView.visibility = View.GONE
+                R.id.loginFragment ->viewBinding.bottomNavigationView.visibility = View.GONE
+                R.id.detailsFragment ->viewBinding.bottomNavigationView.visibility = View.GONE
+                else -> viewBinding.bottomNavigationView.visibility = View.VISIBLE
+            }
         }
+
+        onBackPressedDispatcher.addCallback(object :OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                viewBinding.fragmentContainerView.findNavController().navigateUp()
+            }
+        })
+
     }
 }
