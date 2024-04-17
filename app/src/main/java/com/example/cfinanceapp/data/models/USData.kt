eedb2a55d91d3aeb5b6cf0c7da369be7142418b1
Nick Stavropoulos <stauropoulosn@gmail.com>
2024-04-17 11:30:55 +0200

    API und Data models implementiert

--- app/src/main/java/com/example/cfinanceapp/data/models/USData.kt
+++ app/src/main/java/com/example/cfinanceapp/data/models/USData.kt
@@ -3,7 +3,7 @@ package com.example.cfinanceapp.data.models
 import com.squareup.moshi.Json
 
 data class USData(
-    val price : Int,
+    val price : Double,
     @Json(name = "percent_change_1h")
     val percentChange1h: Double,
     @Json(name = "percent_change_24h")
